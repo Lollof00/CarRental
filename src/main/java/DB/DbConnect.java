98971e6ee2c4dfb510ade0c8cedb19601a6613ea
lollof00 <lorenzofranco80@gmail.com>
2022-05-24 12:15:44 +0200

    connessione

--- src/main/java/DB/DbConnect.java
+++ src/main/java/DB/DbConnect.java
@@ -1,4 +1,28 @@
 package DB;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbConnect {
+
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/CarRental?user=lollo&password=lorenzo00");
+
+            if(connection!=null){
+                System.out.println("Si");
+            }else {
+                System.out.println("No");
+            }
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
